c04ab0d3419b57f66dd318254ee3fdc0d94b8262
Caleb <choover11@gmail.com>
2020-11-17 10:10:50 -0800

    improved tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const should = require('chai').should();
 const Interpreter = require('../interpreter.js');
 const inter = new Interpreter();
 
-describe('Operations', function () {
+describe('syntax', function () {
   it('Addition', function () {
     inter.parse('2 + 3').evaluate().should.equal(5);
     inter.parse('2 + 1 + 3').evaluate().should.equal(6);
@@ -20,7 +20,15 @@ describe('Operations', function () {
   });
 
   it('Division', function () {
-    inter.parse('2 * 3').evaluate().should.equal(6);
+    inter.parse('6 / 2').evaluate().should.equal(3);
     inter.parse('9 / 3 / 3').evaluate().should.equal(1);
+  });
+
+  it('Parantheses', function () {
+    inter.parse('(2 + 3) * 5').evaluate().should.equal(25);
+    inter.parse('(5 + 2)(8 + 9 * 2)').evaluate().should.equal(182);
+    inter.parse('8 - (5 - 2)').evaluate().should.equal(5);
+    inter.parse('(5 - 2)^2').evaluate().should.equal(9);
+    inter.parse('(5 - 2)^(1 + 1)').evaluate().should.equal(9);
   });
 });
